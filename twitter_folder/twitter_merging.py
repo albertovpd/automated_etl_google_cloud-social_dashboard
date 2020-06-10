8304f6c0848f37c66487e4e59c7ebcda4620059c
albertovpd <a.vargas.pina@gmail.com>
2020-06-10 17:25:35 +0200

    twitter added

--- twitter_folder/twitter_merging.py
+++ twitter_folder/twitter_merging.py
@@ -1,13 +1,30 @@
 import pandas as pd 
 import datetime
 
+# it is really expensive to work with Twitter API for
+# monitoring keywords weekly and it is out of project
+# (we are simple mortals with limited budget). Nevertheless
+# the code of monitoring Twitter with Python will be
+# available soon, thanks to Patri
 
-twitter_deshaucio=pd.read_csv("../input/defdesa.csv")
+
+# This is just getting the right shape to use it in 
+# DataStudio.
+
+twitter_deshaucio=pd.read_csv("./input/defdesa.csv")
 deshaucio=twitter_deshaucio[["date","query"]].copy()
-deshaucio
 
-#print(deshaucio)
+twitter_comedor=pd.read_csv("./input/dfcom.csv")
+comedor=twitter_comedor[["date","query"]].copy()
 
-twitter_comedor=pd.read_csv("../input/defdesa.csv")
+twitter_desempleo=pd.read_csv("./input/dfdes.csv")
+desempleo=twitter_comedor[["date","query"]].copy()
 
-print(deshaucio[date].apply(just_date))
+twitter_erte=pd.read_csv("./input/dferte.csv")
+erte=twitter_erte[["date","query"]].copy()
+
+twitter_sepe=pd.read_csv("./input/dfsepe.csv")
+sepe=twitter_sepe[["date","query"]].copy()
+
+twitter_final
+twitter_final.to_csv("./input/twitter_final.csv")
